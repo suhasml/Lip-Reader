a440bd694469ae1409d3ef687546bf2d4d8034e8
Suhas M L <77441851+suhasml@users.noreply.github.com>
2023-10-21 10:21:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+# Import all of the dependencies
+import streamlit as st
+import os 
+import imageio 
+
+import tensorflow as tf 
+from utils import load_data, num_to_char
+from modelLoad import load_model
+
+
+st.set_page_config(layout='wide', page_title='Lip Reader')
+
+
+# Setup the sidebar
+with st.sidebar: 
+    st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
+    st.title('Lip Reader')
+   
+
+st.title('Lip Reader') 
+# Generating a list of options or videos 
+options = os.listdir(os.path.join('data', 's1'))
+selected_video = st.selectbox('Choose video', options)
+
+# Generate two columns 
+col1, col2 = st.columns(2)
+
+if options: 
+
+    # Rendering the video 
+    with col1: 
+        st.info('The input video')
+        file_path = os.path.join('data','s1', selected_video)
+        os.system(f'ffmpeg -i {file_path} -vcodec libx264 test_video.mp4 -y')
+
+        # Rendering inside of the app
+        video = open('test_video.mp4', 'rb') 
+        video_bytes = video.read() 
+        st.video(video_bytes)
+
+
+    with col2: 
+        video = load_data(tf.convert_to_tensor(file_path))
+      
+        # st.info('This is the output of the machine learning model as tokens')
+        model = load_model()
+        yhat = model.predict(tf.expand_dims(video, axis=0))
+        decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
+       
+        # Convert prediction to text
+        st.info('Predicted Caption')
+        converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
+        st.text(converted_prediction)
+        
